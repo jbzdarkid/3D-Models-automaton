b2bdf00cf0b25eb87a90620795450af6581f41db
jbzdarkid <jbzdarkid@gmail.com>
2023-11-19 04:16:09 -0800

    Sigscans + new mem code started working

--- HLMVModel.py
+++ HLMVModel.py
@@ -1,19 +1,33 @@
 from math import sin, cos, radians
 from subprocess import Popen, PIPE
 
-def mem(*params):
+mem_offsets = {}
+def mem(command, *args):
   """
   Communicate with the C memory management utility via subprocess.Popen
   Takes in a series of arguments as parameters for mem.exe
   Returns parsed response, raises all exceptions
   """
-  params = ['mem.exe'] + [str(param) for param in params]
+  if command == 'sigscan':
+    params = ['mem.exe', 'sigscan', *args]
+  elif not args:
+    params = ['mem.exe', 'read', str(mem_offsets[command])]
+  else:
+    params = ['mem.exe', 'write', str(mem_offsets[command])]
+    for arg in args:
+      params.append(bytes.hex(int.to_bytes(arg, byteorder='little')))
+    print(params)
+
   proc = Popen(params, stdout=PIPE, stderr=PIPE, universal_newlines=True)
   stdout, stderr = proc.communicate()
   if stderr:
     raise Exception(stderr)
   if stdout:
-    return [float(o) for o in stdout.split(' ')]
+    if params[1] == 'read':
+      return [float(o) for o in stdout.split(' ')]
+    elif params[1] == 'sigscan':
+      print(stdout)
+      return [bytes.fromhex(line) for line in stdout.strip().split('\n')]
 
 class HLMVModel(object):
   def __init__(self, initial):
@@ -22,6 +36,35 @@ class HLMVModel(object):
     and load the current rotation and translation from memory.
     If initial values are specified, rot and trans will be set instead.
     """
+
+    # TODO explain
+    sigscans = mem('sigscan', '81CA00010000803D')
+
+    mem_offsets['nm'] = int.from_bytes(sigscans[0][8:12], byteorder='little') - 0x410000
+
+    print(mem_offsets)
+
+    mem('nm', 1)
+
+    # int32_t normals = *(int32_t*)&data[index + 8];
+    exit()
+
+    self.mem_offsets = {
+      'rot': 0x23C4B0, # Absolute Rotation
+      'trans': 0x23C4C0, # Absolute Translation
+      'color': 0x23F1B4, # Background color
+      'bg': 0x23F17C, # Enable Background
+      'nm': 0x23F12F, # Normal Maps
+    }
+    """
+  } else if (strcmp(argv[1], "spec") == 0) { // Specular
+    offset = (LPVOID)(base_addr + 0x23F131);
+  } else if (strcmp(argv[1], "ob") == 0) { // Overbrightening
+    offset = (LPVOID)(base_addr + 0x23F1DE);
+  } else if (strcmp(argv[1], "lrot") == 0) { // Light Rotation
+    offset = (LPVOID)(base_addr + 0x23F148);    }
+    """
+
     mem('nm', 1)
     mem('color', '1.0', '1.0', '1.0', '1.0')
     if initial['rotation']:
--- mem/mem.cc
+++ mem/mem.cc
@@ -1,127 +1,114 @@
+/*
 #include <stdio.h> // fprintf
 #include <stdint.h> // uintptr_t
 #include <windows.h> // everything else
 #include <psapi.h> // Module processing. Has to be after windows.h
 #include <tlhelp32.h> // getting the PID
+*/
 
-// http://stackoverflow.com/q/1387064
+#define _CRT_SECURE_NO_WARNINGS
+#include "Memory.h"
+#include <Windows.h>
+
+// https://stackoverflow.com/q/1387064
+// Move this to Memory to replace cassert?
 void throwError() {
-  wchar_t message[256];
-  FormatMessageW(4096, NULL, GetLastError(), 1024, message, 256, NULL);
-  fprintf(stderr, "Error: %ls\n", message);
-  exit(EXIT_FAILURE);
+    wchar_t message[256];
+    FormatMessageW(4096, NULL, GetLastError(), 1024, message, 256, NULL);
+    fprintf(stderr, "Error: %ls\n", message);
+    exit(EXIT_FAILURE);
 }
 
-// https://github.com/erayarslan/WriteProcessMemory-Example
-// http://stackoverflow.com/q/32798185
-// http://stackoverflow.com/q/36018838
+std::vector<uint8_t> ParseHexString(const char* str) {
+    size_t length = strlen(str) / 2; // 1 byte per 2 hex characters
+    std::vector<uint8_t> bytes(length);
+    for (int j = 0; j < length; j++) {
+        int32_t scan = 0;
+        int32_t retval = sscanf(&str[j*2], "%02X", &scan);
+        if (retval != 1 || scan < 0x00 || scan > 0xFF) {
+            fprintf(stderr, "Error: Failed to parse hex string %s", str);
+            exit(EXIT_FAILURE);
+        }
+        bytes[j] = scan;
+    }
+    return bytes;
+}
+
+void PrintHexString(const std::vector<uint8_t>& bytes) {
+    for (const uint8_t byte : bytes) {
+        printf("%02X", byte);
+    }
+}
+
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: mem <trans|rot> [x y z]\n");
-    exit(EXIT_FAILURE);
-  }
-  DWORD pid;
-  HMODULE module_list[1024];
-  DWORD num_results;
-  char name[64];
-  uintptr_t base_addr;
-  PROCESSENTRY32 entry;
-  entry.dwSize = sizeof(entry);
-  HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (argc <= 2) {
+        fprintf(stderr, "Usage:\n");
+        fprintf(stderr, "mem sigscan [hex bytes]\n");
+        fprintf(stderr, "mem read [num bytes] [hex address]");
+        fprintf(stderr, "mem write [hex address] [hex bytes]");
+        exit(EXIT_FAILURE);
+    }
 
-  while (Process32Next(snapshot, &entry)) {
-    if (wcscmp(entry.szExeFile, L"hlmv.exe") == 0) {
-      pid = entry.th32ProcessID;
-      break;
-    }
-  }
+    if (strcmp(argv[1], "sigscan") == 0) {
+        Memory memory(L"hlmv.exe");
+        for (int i = 2; i < argc; i++) {
+            memory.AddSigScan(ParseHexString(argv[i]), [&](int64_t offset, int index, const std::vector<uint8_t>& data) {
+                auto start = data.begin() + index;
+                auto end = start + min(100, data.size() - index); // 100 bytes or end of data
+                PrintHexString(std::vector<uint8_t>(start, end));
+                printf("\n");
+            });
+        }
 
-  HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-  if (!handle) {
-    fprintf(stderr, "Couldn't find HLMV. Is it open?\n");
-    exit(EXIT_FAILURE);
-  }
-  EnumProcessModules(handle, module_list, sizeof(module_list) / sizeof(HMODULE), &num_results);
-  for (int i = 0; i < num_results / sizeof(HMODULE); i++) {
-    GetModuleBaseNameA(handle, module_list[i], name, sizeof(name));
-    if (strcmp(name, "hlmv.exe") == 0) {
-      base_addr = (uintptr_t)module_list[i];
-      break; // All that setup just to get the base address and handle
-    }
-  }
+        memory.ExecuteSigScans();
 
-  // Offsets as of 2022-10-01
-  LPVOID offset;
-  if (strcmp(argv[1], "rot") == 0) { // Absolute Rotation
-    offset = (LPVOID)(base_addr + 0x23C4B0);
-  } else if (strcmp(argv[1], "trans") == 0) { // Absolute Translation
-    offset = (LPVOID)(base_addr + 0x23C4C0);
-  } else if (strcmp(argv[1], "color") == 0) { // Background color
-    offset = (LPVOID)(base_addr + 0x23F1B4);
-  } else if (strcmp(argv[1], "bg") == 0) { // Enable Background
-    offset = (LPVOID)(base_addr + 0x23F17C);
-  } else if (strcmp(argv[1], "nm") == 0) { // Normal Maps
-    offset = (LPVOID)(base_addr + 0x23F12F);
-  } else if (strcmp(argv[1], "spec") == 0) { // Specular
-    offset = (LPVOID)(base_addr + 0x23F131);
-  } else if (strcmp(argv[1], "ob") == 0) { // Overbrightening
-    offset = (LPVOID)(base_addr + 0x23F1DE);
-  } else if (strcmp(argv[1], "lrot") == 0) { // Light Rotation
-    offset = (LPVOID)(base_addr + 0x23F148);
-  } else {
-    fprintf(stderr, "Unknown type parameter: %s\n", argv[1]);
-    exit(EXIT_FAILURE);
-  }
+    } else if (strcmp(argv[1], "read") == 0) {
+        Memory memory(L"hlmv.exe");
 
-  if (argc == 2) {
-    if (strcmp(argv[1], "bg") == 0) {
-      int background;
-      if (ReadProcessMemory(handle, offset, &background, sizeof(background), NULL)) {
-        fprintf(stdout, "%d", background);
-      } else {
-        throwError();
-      }
-    } else if (
-      strcmp(argv[1], "trans") == 0 ||
-      strcmp(argv[1], "rot") == 0 ||
-      strcmp(argv[1], "lrot") == 0
-    ) {
-      float values[3];
-      if (ReadProcessMemory(handle, offset, &values, sizeof(values), NULL)) {
-        fprintf(stdout, "%f %f %f\n", values[0], values[1], values[2]);
-      } else {
-        throwError();
-      }
-    } else if (strcmp(argv[1], "color") == 0) {
-      float values[4];
-      if (ReadProcessMemory(handle, offset, &values, sizeof(values), NULL)) {
-        fprintf(stdout, "%f %f %f %f\n", values[0], values[1], values[2], values[3]);
-      } else {
-        throwError();
-      }
+        int32_t numBytes = 0;
+        int retval = sscanf(argv[2], "%d", &numBytes);
+        if (retval != 1) {
+            fprintf(stderr, "Could not parse number of bytes to read: '%s'", argv[2]);
+            exit(EXIT_FAILURE);
+        }
+
+        std::vector<int64_t> offsets;
+        for (int i = 3; i < argc; i++) {
+            int64_t offset = 0;
+            int retval = sscanf(argv[i], "%lld", &offset);
+            if (retval != 1) {
+                fprintf(stderr, "Could not parse offset %d: '%s'", (i - 3), argv[i]);
+                exit(EXIT_FAILURE);
+            }
+
+            offsets.push_back(offset);
+        }
+
+        PrintHexString(memory.ReadData<uint8_t>(offsets, numBytes));
+
+    } else if (strcmp(argv[1], "write") == 0) {
+        Memory memory(L"hlmv.exe");
+
+        std::vector<int64_t> offsets;
+        int64_t offset = 0;
+        int retval = sscanf(argv[2], "%lld", &offset);
+        if (retval != 1) {
+            fprintf(stderr, "Could not parse offset: '%s'", argv[2]);
+            exit(EXIT_FAILURE);
+        }
+
+        offsets.push_back(offset);
+
+        if (argc < 3) {
+            fprintf(stderr, "Missing write bytes (argument 3)");
+            exit(EXIT_FAILURE);
+        }
+
+        std::vector<uint8_t> bytes = ParseHexString(argv[3]);
+        memory.WriteData<uint8_t>(offsets, bytes);
+
+    } else {
+        fprintf(stderr, "Unknown command '%s'", argv[1]);
+        exit(EXIT_FAILURE);
     }
-  } else if (argc == 3) {
-    int background = atoi(argv[2]);
-    if (!WriteProcessMemory(handle, offset, &background, sizeof(background), NULL)) {
-      throwError();
-    }
-  } else if (argc == 5) {
-    float values[3];
-    values[0] = atof(argv[2]);
-    values[1] = atof(argv[3]);
-    values[2] = atof(argv[4]);
-    if (!WriteProcessMemory(handle, offset, &values, sizeof(values), NULL)) {
-      throwError();
-    }
-  } else if (argc == 6) {
-    float values[4];
-    values[0] = atof(argv[2]);
-    values[1] = atof(argv[3]);
-    values[2] = atof(argv[4]);
-    values[3] = atof(argv[5]);
-    if (!WriteProcessMemory(handle, offset, &values, sizeof(values), NULL)) {
-      throwError();
-    }
-  }
-  CloseHandle(handle);
 }
