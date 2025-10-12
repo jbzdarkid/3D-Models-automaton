13bc435061f79b54d1cb5603907110c62586febf
Joseph Blackman <jbzdarkid@gmail.com>
2025-10-12 01:50:58 -0700

    missed one

--- make_release.py
+++ make_release.py
@@ -14,9 +14,9 @@ def zip_repository():
     z.write('imageprocessor.py')
     z.write('mem.exe')
 
+    z.write('TFWiki-scripts/requirements.txt')
     z.write('TFWiki-scripts/wikitools/wiki.py')
     z.write('TFWiki-scripts/wikitools/page.py')
-    z.write('TFWiki-scripts/wikitools/requirements.txt')
 
     z.write('3DModels_BlackFirePlusBlackBackground/HLMV - Cubemap Fix.vpk')
     z.write('3DModels_BlackFirePlusBlackBackground/HLMV - Fire Overlay Fix.vpk')
