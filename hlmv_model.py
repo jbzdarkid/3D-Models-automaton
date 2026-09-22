926ce4a24587d4de41632a2842fb8616aa04f0aa
mallory <aphrodisiac@gmail.com>
2026-09-22 12:57:35 -0400

    fix model reset (#8)

--- hlmv_model.py
+++ hlmv_model.py
@@ -239,3 +239,11 @@ class HLMVModel():
       self.translation[1] + sin(y)*self.rot_offset + xy_shift,
       self.translation[2] - sin(x)*self.rot_offset
     ), self.mem_offsets['trans'])
+
+  def reset(self):
+    """
+    Reset the model to its initial rotation.
+    """
+
+    mem('write', pack('fff', *self.rotation), self.mem_offsets['rot'])
+    mem('write', pack('fff', *self.translation), self.mem_offsets['trans'])
